0e13dba1024924e59cd889fa3b86a7d49259d4d4
Matteo Agostinelli <agostinelli@gmail.com>
2009-07-31 13:10:38 +0200

    Working Gnuplot backend

--- src/gnuplotbuilder.cpp
+++ src/gnuplotbuilder.cpp
@@ -19,32 +19,95 @@
 */
 #include "gnuplotbuilder.h"
 #include "externalprocess.h"
+#include "latexprocess.h"
+
 #include <KTextEditor/Document>
+#include <KTemporaryFile>
 #include <QRegExp>
 #include <QFile>
+#include <QFileInfo>
+#include <QDir>
 
 GnuplotBuilder::GnuplotBuilder(KTextEditor::Document* doc, const QString& origDir, QObject* parent): GraphicsBuilder(doc, origDir, parent)
 {
+	m_tempFile = new KTemporaryFile;
+	m_tempFile->setPrefix(m_workingDir->absolutePath() + "/");
+	m_tempFile->setSuffix(".gp");
+	
+	m_tempFile->open();
+	m_tempFileInfo = new QFileInfo(m_tempFile->fileName());
+}
 
+GnuplotBuilder::~GnuplotBuilder()
+{
+	delete m_tempFile;
+	delete m_tempFileInfo;
 }
 
 bool GnuplotBuilder::generateFormat(const QString& extension)
 {
 	if (extension == "pdf")
+	{
+		if (!generateDvi())
+			return false;
+		if (!generateEps())
+			return false;
+		if (!generatePdf())
+			return false;
+	}
+	else if (extension.contains("eps"))
+	{
+		if (!generateDvi())
+			return false;
+		if (!generateEps())
+			return false;
+	}
+	else if (extension == "dvi")
+	{
 		return generateDvi();
+	}
+	else if (extension == "png")
+	{
+		if (!generateDvi())
+			return false;
+		if (!generateEps())
+			return false;
+		if (!generatePdf())
+			return false;
+		if (!generatePng())
+			return false;
+	}
+	else if (extension == "svg")
+	{
+		if (!generateDvi())
+			return false;
+		if (!generateEps())
+			return false;
+		if (!generatePdf())
+			return false;
+		if (!generateSvg())
+			return false;
+	}
+	else
+		return false;
 	
-	return generateDvi();
+	return true;
 }
 
 bool GnuplotBuilder::generateDvi()
 {
+	QTextStream out(m_tempFile);
+		
 	ExternalProcess gnuplot("gnuplot");
 	QStringList gnuplot_args;
+	gnuplot_args << m_tempFileInfo->fileName();
 	
 	QRegExp regex1("[\'\"]([\\w\\./\\-]+)[\'\"]");
 	
 	QStringList origFileNames;
 	QStringList lines = m_doc->text().split("\n");
+	
+	QString gnuplot_outputfile;
 	foreach (QString line, lines)
 	{
 		int pos = 0;
@@ -53,23 +116,45 @@ bool GnuplotBuilder::generateDvi()
 			QString capture = regex1.cap(1);
 			
 			if (QFile::exists(m_origDir + "/" + capture))
-				origFileNames << capture;
+			{
+				if (!line.startsWith("set output"))
+				{
+					origFileNames << capture;
+					line = line.replace(capture, QString("%1/%2").arg(m_origDir).arg(capture));
+				}
+				else
+					gnuplot_outputfile = capture;
+			}
 			pos += regex1.matchedLength();
 		}
+		out << line + "\n";
 	}
+	m_tempFile->close();
 	
-	qDebug() << origFileNames;
-	QString gnuplot_input = m_doc->text();
-	
-	foreach (QString fileName, origFileNames)
-		gnuplot_input = gnuplot_input.replace(fileName, QString("builddir/%1").arg(fileName));
-	
-		
-	//qDebug() << gnuplot_input;
-// 	if (!gnuplot.startWith(m_doc->text(), gnuplot_args))
-// 		emit applicationError(gnuplot.appName(), gnuplot.readAllStandardError());
+	qDebug() << origFileNames;			
+	qDebug() << gnuplot_outputfile;
+
+	if (!gnuplot.startWith("", gnuplot_args))
+ 		emit applicationError(gnuplot.appName(), gnuplot.readAllStandardError());
 	
 	QString gnuplot_out = gnuplot.readAllStandardOutput();
+	
+	LatexProcess latexProcess(m_tempFileInfo->baseName());
+	//QString latexDoc = QString("\\documentclass{article}\n\\begin{document}\n%1\n\\end{document}\n").arg("Hello");
+	QString latexDoc = "\\documentclass{article}\n"
+	"\\usepackage{graphicx}\n"
+	"\\pagestyle{empty}\n"
+	"\\thispagestyle{empty}\n"
+	"\\begin{document}\n"
+	"\\newbox\\graph\n"
+	"\\input " + gnuplot_outputfile + "\n"
+	"\\box\n"
+	"\\graph\n"
+	"\\end{document}\n";
+	
+	return latexProcess.build(latexDoc);
 }
+
+
 
 
--- src/gnuplotbuilder.h
+++ src/gnuplotbuilder.h
@@ -27,6 +27,7 @@ class GnuplotBuilder : public GraphicsBuilder
 {
 	public:
 		GnuplotBuilder(KTextEditor::Document* doc, const QString& origDir = "", QObject* parent = 0);
+		~GnuplotBuilder();
 		virtual bool generateFormat(const QString& extension);
 		
 	private:
