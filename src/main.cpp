2cff8c503309fb7e8e57a44d120c1a39be5cb879
Matteo Agostinelli <agostinelli@gmail.com>
2010-09-10 10:41:14 +0200

    Fixed copyright and version

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 /***************************************************************************
-*   Copyright (C) 2009 by Matteo Agostinelli                              *
+*   Copyright (C) 2010 by Matteo Agostinelli                              *
 *   agostinelli@gmail.com                                                 *
 *                                                                         *
 *   This program is free software; you can redistribute it and/or modify  *
@@ -29,12 +29,12 @@
 
 int main (int argc, char *argv[])
 {
-    KAboutData aboutData( "cirkuit", "Cirkuit", ki18n("Cirkuit"), "0.2.90", ki18n("An application to generate publication-ready figures. It is a KDE frontend for both Circuit Macros by J. D. Aplevich and for the TikZ language. <p>Visit the <a href=http://www.ece.uwaterloo.ca/~aplevich/Circuit_macros>Circuit Macros</a> and <a href=http://www.texample.net/tikz/>TikZ</a> websites for further information."), KAboutData::License_GPL, ki18n("(c) 2009 Matteo Agostinelli"));
+    KAboutData aboutData( "cirkuit", "Cirkuit", ki18n("Cirkuit"), "0.2.80", ki18n("An application to generate publication-ready figures. It is a KDE frontend for both Circuit Macros by J. D. Aplevich and for the TikZ language. <p>Visit the <a href=http://www.ece.uwaterloo.ca/~aplevich/Circuit_macros>Circuit Macros</a> and <a href=http://www.texample.net/tikz/>TikZ</a> websites for further information."), KAboutData::License_GPL, ki18n("(c) 2009 Matteo Agostinelli"));
     aboutData.addAuthor(ki18n("Matteo Agostinelli"), ki18n("Maintainer"), "agostinelli@gmail.com");
     KCmdLineArgs::init( argc, argv, &aboutData );
 
     KCmdLineOptions options;
-    options.add("+[file]", ki18n("Document to open")); //new
+    options.add("+[file]", ki18n("Document to open"));
     KCmdLineArgs::addCmdLineOptions( options );
 
     KApplication app;
