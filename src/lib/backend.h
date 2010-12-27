c1e6ce6c5b8c31d2c62ca8df6bf8c5ce96b7b92f
Matteo Agostinelli <agostinelli@gmail.com>
2010-12-27 15:55:41 +0100

    Fixed copyrights

--- src/lib/backend.h
+++ src/lib/backend.h
@@ -1,6 +1,5 @@
 /*
-    <one line to give the program's name and a brief idea of what it does.>
-    Copyright (C) 2010  Matteo Agostinelli
+    Copyright (C) 2011 Matteo Agostinelli <agostinelli@gmail.com>
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
--- src/lib/cirkuit_export.h
+++ src/lib/cirkuit_export.h
@@ -1,5 +1,5 @@
 /*
-    Copyright (C) 2009  Matteo Agostinelli <agostinelli@gmail.com>
+    Copyright (C) 2011 Matteo Agostinelli <agostinelli@gmail.com>
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
