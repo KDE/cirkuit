30bee5e4d7a35c788ff6d4d64d4073d5754e9fc7
Matteo Agostinelli <agostinelli@gmail.com>
2010-12-28 10:33:57 +0100

    Improved Backend API (based on Cantor)

--- src/lib/backend.cpp
+++ src/lib/backend.cpp
@@ -18,9 +18,155 @@
 
 #include "backend.h"
 
+#include <QStringList>
+
+#include <KUrl>
+#include <KDebug>
+#include <KServiceTypeTrader>
+#include <KService>
+#include <KPluginInfo>
+
+class Cirkuit::BackendPrivate {
+public:
+    QString name;
+    QString comment;
+    QString icon;
+    KUrl url;
+};
+
 using namespace Cirkuit;
 
-Backend::Backend(QObject* parent): QObject(parent)
+Backend::Backend(QObject* parent, const QList<QVariant>& args): QObject(parent), d(new BackendPrivate)
 {
+    Q_UNUSED(args);
+}
+
+Backend::~Backend()
+{
+    delete d;
+}
+
+
+bool Backend::checkRequirements() const
+{
+    return false;
+}
+
+QString Backend::name() const
+{
+    return d->name;
+}
+
+QString Backend::comment() const
+{
+    return d->comment;
+}
+
+QString Backend::description() const
+{
+    return comment();
+}
+
+QString Backend::icon() const
+{
+    return d->icon;
+}
+
+KUrl Backend::url() const
+{
+    return d->url;
+}
+
+KUrl Backend::helpUrl() const
+{
+    return KUrl();
+}
+
+QWidget* Backend::settingsWidget(QWidget* parent) const
+{
+    Q_UNUSED(parent)
+    return 0;
+}
+
+KConfigSkeleton* Backend::config() const
+{
+    return 0;
+}
+
+int Backend::initialLineNumber() const
+{
+    return 0;
+}
+
+QString Backend::initialText() const
+{
+    return QString();
+}
+
+static QList<Backend*> backendCache;
+
+QStringList Backend::listAvailableBackends()
+{
+    QList<Backend* > backends=availableBackends();
+    QStringList l;
+    foreach (Backend* b, backends) {
+        if (b->checkRequirements()) {
+            l << b->name();
+        }
+    }
+
+    return l;
+}
+
+QList<Backend*> Backend::availableBackends()
+{
+    //if we already have all backends Cached, just return the cache.
+    //otherwise create the available backends
+    if (!backendCache.isEmpty()) {
+        return backendCache;
+    }
+
+    KService::List services;
+    KServiceTypeTrader* trader = KServiceTypeTrader::self();
+
+    services = trader->query("Cirkuit/Backend");
+    
+    KService::List::const_iterator iter;
+    for (iter = services.begin(); iter < services.end(); ++iter) {
+        QString error;
+        KService::Ptr service = *iter;
+        
+        KPluginFactory *factory = KPluginLoader(service->library()).factory();
+        if (!factory) {
+            kError(5001) << "error: " << error;
+            continue;    
+        }
+        
+        Backend* backend = factory->create<Backend>(0);
+        if (!backend) {
+            kDebug() << "error: " << error;
+            continue;
+        }        
+   
+        KPluginInfo info(service);
+        backend->d->name = info.name();
+        backend->d->comment = info.comment();
+        backend->d->icon = info.icon();
+        backend->d->url = info.website();
+        backendCache << backend;
+    }
+    return backendCache;
+}
+
+Backend* Backend::createBackend(const QString& name)
+{
+    QList<Backend*> backends=availableBackends();
+    foreach(Backend* b, backends) {
+        if (QString::compare(b->name(), name, Qt::CaseInsensitive) == 0 || QString::compare(b->id(), name, Qt::CaseInsensitive) == 0) {
+            if (b->checkRequirements()) return b;
+        }
+    }
+
+    return 0;
 }
 
--- src/lib/backend.h
+++ src/lib/backend.h
@@ -22,14 +22,101 @@
 #include "cirkuit_export.h"
 
 #include <QtCore/QObject>
+#include <QVariant>
+
+class KUrl;
+class KConfigSkeleton;
 
 namespace Cirkuit
 {
+class BackendPrivate;
     
 class CIRKUIT_EXPORT Backend : public QObject
 {
+    Q_OBJECT
+protected:
+    explicit Backend(QObject* parent = 0, const QList<QVariant>& args = QList<QVariant>());
+    virtual ~Backend();
+    
 public:
-    Backend(QObject* parent = 0);
+    virtual bool checkRequirements() const;
+    
+    /**
+     * Returns a unique string to identify this backend.
+     * @return string to identify backend
+     */
+    virtual QString id() const = 0;
+    
+    //Stuff extracted from the .desktop file
+    /**
+     * Returns the name of the backend
+     * @return the backends name
+     */
+    QString name() const;
+    /**
+     * Returns a short comment about the backend.
+     * @return comment about the backend
+     */
+    QString comment() const;
+    /**
+     * Returns the icon to use with this backend
+     * @return name of the icon
+     */
+    QString icon() const;
+    /**
+     * Returns the Url of the Homepage for the Backend
+     * @return the url
+     */
+    KUrl url() const;
+    /**
+     * Returns an Url pointing to the Help of the Backend
+     * The method should be overwritten by all Backends(who have an online help)
+     * @return Url of the help
+     */
+    virtual KUrl helpUrl() const;
+    
+    /**
+     * Returns a longer description of the Backend, e.g. purpose, strengths etc.
+     * It should help the user to decide between the different Backends
+     * @return a description of the backend. It can contain html
+     */
+    virtual QString description() const;
+    /**
+     * Returns a Widget for configuring this backend
+     * @return Widget for usage in the Settings dialog
+     */
+    virtual QWidget* settingsWidget(QWidget* parent) const;
+    /**
+     * Returns a KConfig object, containing all the settings,
+     * the backend might need
+     * @return a KConfigSkeleton object, for configuring this backend
+     */
+    virtual KConfigSkeleton* config() const;
+    
+    virtual QString initialText() const;    
+    virtual int initialLineNumber() const;    
+    
+    /**
+     * Returns a list of the names of all the installed and enabled backends
+     * @return a list of the names of all the installed and enabled backends
+     * @see isEnabled()
+     */
+    static QStringList listAvailableBackends();
+    /**
+     * Returns Pointers to all the installed backends
+     * @return Pointers to all the installed backends
+     */
+    static QList<Backend*> availableBackends();
+    /**
+     * Returns the backend with the given name, or null if it isn't found
+     * @return the backend with the given name, or null if it isn't found
+     */
+    static Backend* createBackend(const QString& name);
+    
+    static Backend* autoChooseBackend(const QString& text);
+    
+  private:
+    BackendPrivate* d;
 };
 
 }
