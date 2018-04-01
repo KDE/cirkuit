cdc3e8926a80646403828036ff43d7db213b216f
Kai Morwinski <morckman@gmail.com>
2018-04-01 23:21:19 +0200

    Port cirkuit's main.cpp to KF5
    Summary:
    The following deprecated classes are ported to their Qt/KF5 counterparts:
    - KApplication -> QApplication
    - K4AboutData -> KAboutData
    - KCmdLinArgs -> QCommandLineParser
    
    Translation now uses i18n(str) from KLocalizedString instead of ki18n(str).
    
    Strings for the commandline parser are adjusted to fit current
    kde scheme:
    - '+' removed from argument syntax
    - '.' added at the end of the argument description
    
    The string parameter shortDescription passed to K(4)AboutData got
    split up into shortDescription and otherText.
    The html markup contained in this param looked really ugly in the
    commandline output.
    Now only the short markup-free text is displayed on the commandline,
    while in the about dialog (visually) nothing has changed.
    
    Reviewers: asturmlechner
    
    Reviewed By: asturmlechner
    
    Differential Revision: https://phabricator.kde.org/D10999

--- src/main.cpp
+++ src/main.cpp
@@ -18,32 +18,52 @@
 *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.          *
 ***************************************************************************/
 
-#include <KApplication>
-#include <K4AboutData>
-#include <KCmdLineArgs>
+#include <KAboutData>
+#include <QApplication>
+#include <QCommandLineParser>
+#include <KLocalizedString>
 
 #include "cirkuitconfig.h"
 #include "mainwindow.h"
 
 int main (int argc, char *argv[])
 {
-    K4AboutData aboutData( "cirkuit", "cirkuit", ki18n("Cirkuit"), VERSION, ki18n("An application to generate publication-ready figures. It is a KDE frontend for Circuit Macros by J. D. Aplevich, TikZ and Gnuplot. <p>Visit the <a href=http://www.ece.uwaterloo.ca/~aplevich/Circuit_macros>Circuit Macros</a> and <a href=http://www.texample.net/tikz>TikZ</a> websites for further information."), K4AboutData::License_GPL, ki18n("(c) 2011 Matteo Agostinelli"));
-    aboutData.addAuthor(ki18n("Matteo Agostinelli"), ki18n("Maintainer"), "matteo@agostinelli.me", "http://agostinelli.me");
-    aboutData.setHomepage("http://projects.kde.org/cirkuit");
-    KCmdLineArgs::init( argc, argv, &aboutData );
+    QApplication app(argc, argv); // must be created first for translation to work
 
-    KCmdLineOptions options;
-    options.add("+[file]", ki18n("Document to open"));
-    KCmdLineArgs::addCmdLineOptions( options );
+    /* setup localization */
+    KLocalizedString::setApplicationDomain("cirkuit"); /* connect to translation catalog */
 
-    KApplication app;
+    /* setup application's about-data */
+    KAboutData aboutData(QStringLiteral("cirkuit"), i18n("Cirkuit"), QStringLiteral(VERSION),
+                         i18n("An application to generate publication-ready figures. "
+                              "It is a KDE frontend for Circuit Macros by J. D. Aplevich, TikZ and Gnuplot."),
+                         KAboutLicense::GPL, i18n("(c) 2011 Matteo Agostinelli"),
+                         i18n("Visit the <a href=http://www.ece.uwaterloo.ca/~aplevich/Circuit_macros>Circuit Macros</a> and "
+                              "<a href=http://www.texample.net/tikz>TikZ</a> websites for further information.")
+                         );
+    aboutData.addAuthor(i18n("Matteo Agostinelli"), i18n("Maintainer"),
+                        QStringLiteral("matteo@agostinelli.me"), QStringLiteral("http://agostinelli.me")
+                        );
+    aboutData.setHomepage(QStringLiteral("http://projects.kde.org/cirkuit"));
+    KAboutData::setApplicationData(aboutData); /* install data in already created QApplication instance */
+
+    /* create KDE-typical commandline parser with optional 'file' argument */
+    QCommandLineParser parser;
+    aboutData.setupCommandLine(&parser); /* add description and standard options like version, author, etc. */
+    parser.addPositionalArgument(QStringLiteral("file"), i18n("Document to open."), i18n("[file]")); /* add 'file' arg */
+
+    /* parse the command line */
+    parser.process(app);
+    aboutData.processCommandLine(&parser); /* handle all standard KDE options */
+    const QStringList args = parser.positionalArguments(); /* get all passed optional arguments */
+
+    /* create and show the gui */
     MainWindow* window = new MainWindow();
-
-    KCmdLineArgs *args = KCmdLineArgs::parsedArgs();
-    if (args->count()) {
-        window->loadFile(args->url(0).url());
+    if (!args.isEmpty()) {
+        /* 'file' argument */
+        const QString fileArg = args.at(0); /* only the first one is used, others are ignored */
+        window->loadFile(QUrl::fromLocalFile(fileArg)); /* open passed file and trigger preview generation */
     }
-
     window->show();
 
     return app.exec();
